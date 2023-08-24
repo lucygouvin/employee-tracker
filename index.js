b0c5a5bd2f47507657754ee731d62962f17858d3
lgouvinSchell <141948090+lgouvinSchell@users.noreply.github.com>
2023-08-23 23:58:15 -0400

    added process exit, dotenv, and made walkthrough video

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Import Inquirer and MySQL2
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 // Connect to database
 const db = mysql.createConnection(
+
     {
     host: '127.0.0.1',
     // MySQL username,
-    user: 'root',
+    user: process.env.DB_USER,
     // MySQL password
-    password: 'rootr00t!',
-    database: 'employees_db'
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     },
     console.log(`Connected to the classlist_db database.`)
     );
@@ -50,7 +53,7 @@ function startProgram(){
         updateEmployee()
         break;
       case "Quit":
-        // do nothing, allow inquirer to end
+        process.exit(0)
         break;
     }
 
